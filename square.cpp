36b8df125c02f810efe9f6826742dc3daa387dbb
JoahnU <148021084+JoahnU@users.noreply.github.com>
2023-11-14 14:51:50 +0000

    square papá

--- square.cpp
+++ square.cpp
@@ -7,13 +7,16 @@ class Square{
   int x2;
   int y1;
   int y2;
+  int px;
+  int py;
+  
   
   
   Square(){
-    x1 = 1;
-    x2 = 2;
-    y1 = 1;
-    y2 = 2;
+    x1 = 0;
+    x2 = 3;
+    y1 = 0;
+    y2 = 3;
   }
   double calc_dist(){
       return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
@@ -30,11 +33,26 @@ class Square{
   int perimeter(){
       return 2 * (dist_x() + dist_y());
   }
+  bool contains(int px, int py){
+     bool contain = true;
+      if(px > x1 && py > y1 || py == y1 && px < x2 || px == x2 && py < y2 || py == y2){
+        return contain;
+      }else{
+        contain = false;
+        return contain;
+      }
+  }
 };
 int main ()
 {
   Square a;
+  int punto_x;
+  int punto_y;
+    cin >> punto_x;
+    cin >> punto_y;
     cout << a.perimeter()<< endl;
-    cout<< a.area();
+    cout << a.area() << endl;
+    cout << a.contains(punto_x, punto_y);
+    
   return 0;
 }
